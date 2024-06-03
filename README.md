22ad9473efe6cb79b51607f9457dfd0dfa3e0f54
Amila <134957961+studytime-ad@users.noreply.github.com>
2024-06-02 21:50:36 -0500

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # exploring-graphql
  
+Install necessary packages:
+pip3 install Flask ariadne Flask-SQLAlchemy
+
+Run application using 
+python app.py
+
+Access app locally via http://127.0.0.1:5000/graphql
+
+Try a test request on graphql UI:
+{
+  posts {
+    id
+  }
+}
---
+++ app.py
@@ -0,0 +1,84 @@
+from ariadne import QueryType, graphql_sync, make_executable_schema
+from ariadne.explorer import ExplorerGraphiQL
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+try:
+    # Attempt to set the database URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
+    db = SQLAlchemy(app)
+except Exception as e:
+    print(f"Error configuring database: {e}")
+    exit(1)
+
+
+type_defs = """
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+    }
+
+    type Query {
+        posts: [Post!]!  # List of non-nullable posts
+    }
+"""
+
+query = QueryType()
+
+@query.field("posts")
+def resolve_posts(obj, info):
+    print(info)
+    return Post.query.all()  # Fetch all posts
+
+schema = make_executable_schema(type_defs, query)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+    
+with app.app_context():
+    db.create_all()  # This will create the table if it doesn't exist
+
+# Retrieve HTML for the GraphiQL.
+# If explorer implements logic dependant on current request,
+# change the html(None) call to the html(request)
+# and move this line to the graphql_explorer function.
+explorer_html = ExplorerGraphiQL().html(None)
+
+
+@app.route("/graphql", methods=["GET"])
+def graphql_explorer():
+    # On GET request serve the GraphQL explorer.
+    # You don't have to provide the explorer if you don't want to
+    # but keep on mind this will not prohibit clients from
+    # exploring your API using desktop GraphQL explorer app.
+    return explorer_html, 200
+
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    # GraphQL queries are always sent as POST
+    data = request.get_json()
+
+    # Note: Passing the request to the context is optional.
+    # In Flask, the current request is always accessible as flask.request
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value={"request": request},
+        debug=app.debug
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    
